7f2f81834d896fe39103fa355a6f53d68182ec6c
陈云川 <chenych@zhejianglab.com>
2023-02-04 16:32:59 +0800

    fix: 🐛 修复检查是否是exFAT格式的文件系统失败问题

--- synocheckshare.sh
+++ synocheckshare.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
 
+# 通过该命令查看被调用的参数
+# echo "$@" >> /tmp/tmp-synocheckshare.txt
+# 目前被调用的参数形如：
+#   --vol-mounted USB /dev/sdq2 /volumeUSB1/usbshare1-2
+#   --vol-unmounting USB /dev/sdq2 /volumeUSB1/usbshare1-2
+
 if [ "$1" == "--vol-mounted" ]; then
-    m=$(fdisk -l | grep "$3" | grep "exFAT")
+    #m=$(fdisk -l | grep "$3" | grep "exFAT")
+    m=$(df -Th "$3" | sed 1d | grep fuseblk)
     if [ -z "$m" ]; then
         /usr/syno/bin/synocheckshare.bin "$@"
     fi
